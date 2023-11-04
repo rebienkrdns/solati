6d32346d7f472e4bfe95278554354f2f12acc887
Neiber Cardenas <rebienkrdns@gmail.com>
2023-11-04 14:57:17 -0500

    feat: api authentication clean: unused files

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required', 'max:255'],
+            'password' => ['required', 'max:255'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $user= Auth::user();
+
+            return ['token' => $user->createToken($user->name)->plainTextToken];
+        }
+
+        return response('Unauthorized.', 401);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Solati test: v' . Application::VERSION . ' PHP v' . PHP_VERSION;
 });
+
+Route::get('/login', function () {
+    return response('Unauthorized.', 401);
+})->name('login');
