607c8a8f58f34958d7076ca5efac555561d58f9d
Neiber Cardenas <rebienkrdns@gmail.com>
2023-11-04 14:31:00 -0500

    feat: users migration and default user 'pruebas'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->string('dni', 15)->nullable();
+			$table->string('phone', 15)->nullable();
+			$table->string('address')->nullable();
+			$table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
